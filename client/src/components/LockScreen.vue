99dc0c899d8e143d2cabd52652d3f508569ee633
p1vz <samgust21@gmail.com>
2018-10-25 23:27:55 +0300

    Прототип заглушки о недоступном сервере

--- client/src/components/LockScreen.vue
+++ client/src/components/LockScreen.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="hide">
-
-  </div>
+  <v-dialog v-model="lockSceenToggle" persistent max-width="300">
+    <v-card dark style="text-align:center;">
+      <v-card-title class="headline justify-center">Ожидание сервера..</v-card-title>
+      <v-card-text>Please stand by & don't close the tab</v-card-text>
+      <v-progress-linear
+        indeterminate
+        color="#E0E0E0"
+        class="mb-0"
+      ></v-progress-linear>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -10,6 +18,11 @@
       return {
 
       };
+    },
+    computed: {
+      lockSceenToggle() {
+        return this.$store.getters['serverStatus']
+      }
     },
 
   }
