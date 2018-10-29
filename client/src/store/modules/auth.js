387a07b6908047af36de3f8a2b86fc1c15ea4be8
Ilya <ilf@bk.ru>
2018-10-29 12:00:31 +0300

    Регистрация во Vuex

---
+++ client/src/store/modules/auth.js
@@ -0,0 +1,20 @@
+import authServices from '../../services/auth';
+
+export default {
+  namespaced: true,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {
+    async checkEmail(context, payload) {
+      return await authServices.emailExist(payload.email).status;
+    },
+    async registration(context, payload) {
+
+      return await authServices.registration({
+        email: payload.email,
+        password: payload.password
+      }).status;
+    }
+  }
+}
