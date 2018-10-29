e23351796799287ff90d26306de62601bc2c4b88
Ilya <ilf@bk.ru>
2018-10-29 17:15:04 +0300

    Рефакторинг старого кода.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
     "semi": [ 2, "always" ],
     "indent": "off",
     "vue/script-indent": ["error", 2, {
-      "baseIndent": 1
+      "baseIndent": 1,
+      "switchCase": 1
     }],
     "vue/no-unused-components": 0,
     "space-before-function-paren": ["error", {
